b897edbe4884782f5085c5d6068521e333dda9b0
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2024-02-01 21:17:45 -0300

    project started

---
+++ src/main/java/com/pedroacbg/togolog/TogologApplication.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.togolog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TogologApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TogologApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroacbg/togolog/TogologApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroacbg.togolog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TogologApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
