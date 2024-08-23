2ca072b429c5f9d1289dd295bc56a6c9b88aaf9a
Pedro Augusto Costa Anjos <pedrooaugustoo130@gmail.com>
2024-08-23 14:24:21 -0300

    product data transfer object

---
+++ src/main/java/com/pedroacbg/togolog/dto/ProductDTO.java
@@ -0,0 +1,53 @@
+package com.pedroacbg.togolog.dto;
+
+import com.pedroacbg.togolog.entities.Category;
+import com.pedroacbg.togolog.entities.Product;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class ProductDTO {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private String imgUrl;
+    private Instant date;
+
+    private List<CategoryDTO> categories = new ArrayList<>();
+
+    public ProductDTO(){}
+
+    public ProductDTO(Long id, String name, String description, Double price, String imgUrl, Instant date) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+        this.date = date;
+    }
+
+    public ProductDTO(Product entity) {
+        this.id = entity.getId();
+        this.name = entity.getName();
+        this.description = entity.getDescription();
+        this.price = entity.getPrice();
+        this.imgUrl = entity.getImgUrl();
+        this.date = entity.getDate();
+    }
+
+    public ProductDTO(Product entity, Set<Category> categories){
+        this(entity);
+        categories.forEach(category -> this.categories.add(new CategoryDTO(category)));
+    }
+
+}
